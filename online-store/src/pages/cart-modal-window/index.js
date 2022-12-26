b9cc5bf94d13ce7abf5ca316e41920df94981920
chorny-znich <vargar.dt@gmail.com>
2022-12-26 22:48:32 +0300

    feat: add check for customer field

--- online-store/src/pages/cart-modal-window/index.js
+++ online-store/src/pages/cart-modal-window/index.js
@@ -1,3 +1,18 @@
 var form = document.forms[0];
 var customerName = form.elements[0];
-console.dir(customerName);
+var a = customerName.value;
+console.dir(customerName.value);
+customerName.addEventListener('focus', function () {
+});
+customerName.addEventListener('blur', function () {
+    var customer = customerName.value;
+    var customerArray = customer.split(' ');
+    if (customerArray.length < 2) {
+        console.log('Customer name error! (2 words or more)');
+    }
+    for (var _i = 0, customerArray_1 = customerArray; _i < customerArray_1.length; _i++) {
+        var item = customerArray_1[_i];
+        if (item.length < 3)
+            console.log('Customer name error! (word\'s length more than 3 symbol)');
+    }
+});
--- online-store/src/pages/cart-modal-window/index.ts
+++ online-store/src/pages/cart-modal-window/index.ts
@@ -1,3 +1,18 @@
 const form = document.forms[0];
-const customerName = form.elements[0];
-console.dir(customerName);
+const customerName = <HTMLInputElement>form.elements[0];
+let a: string = customerName.value;
+console.dir(customerName.value);
+
+customerName.addEventListener('focus', () => {
+})
+customerName.addEventListener('blur', () => {
+  const customer: string = customerName.value;
+  const customerArray: string[] = customer.split(' ');
+  if (customerArray.length < 2) {
+    console.log('Customer name error! (2 words or more)');
+  }
+  for (const item of customerArray) {
+    if (item.length < 3) console.log('Customer name error! (word\'s length more than 3 symbol)');
+  }
+})
+
