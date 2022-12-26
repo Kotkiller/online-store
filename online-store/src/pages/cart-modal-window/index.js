5212c6a05591c3cd7ef6a6b4433641cebc6844fa
chorny-znich <vargar.dt@gmail.com>
2022-12-26 23:53:01 +0300

    feat: add checks for customer phone number field

--- online-store/src/pages/cart-modal-window/index.js
+++ online-store/src/pages/cart-modal-window/index.js
@@ -1,7 +1,7 @@
 var form = document.forms[0];
 var customerName = form.elements[0];
-var a = customerName.value;
-console.dir(customerName.value);
+var customerPhone = form.elements[1];
+console.log(customerPhone);
 customerName.addEventListener('focus', function () {
 });
 customerName.addEventListener('blur', function () {
@@ -15,4 +15,27 @@ customerName.addEventListener('blur', function () {
         if (item.length < 3)
             console.log('Customer name error! (word\'s length more than 3 symbol)');
     }
+});
+// checks for customer phone number field
+customerPhone.addEventListener('focus', function () {
+});
+customerPhone.addEventListener('blur', function () {
+    var phone = customerPhone.value;
+    var firstSymbol = phone[0];
+    if (firstSymbol != '+') {
+        console.log('first symbol not "+');
+    }
+    if ((phone.slice(1)).length < 9) {
+        console.log('phone number less than 9 symbols');
+    }
+    if (isNaN(Number(phone.slice(1)))) {
+        console.log('phone number must contain only symbols');
+    }
+    /*
+    if (customerArray.length < 2) {
+      console.log('Customer name error! (2 words or more)');
+    }
+    for (const item of customerArray) {
+      if (item.length < 3) console.log('Customer name error! (word\'s length more than 3 symbol)');
+    }*/
 });
--- online-store/src/pages/cart-modal-window/index.ts
+++ online-store/src/pages/cart-modal-window/index.ts
@@ -1,8 +1,9 @@
 const form = document.forms[0];
 const customerName = <HTMLInputElement>form.elements[0];
-let a: string = customerName.value;
-console.dir(customerName.value);
+const customerPhone = <HTMLInputElement>form.elements[1];
+console.log(customerPhone);
 
+// checks for customer name field
 customerName.addEventListener('focus', () => {
 })
 customerName.addEventListener('blur', () => {
@@ -15,4 +16,13 @@ customerName.addEventListener('blur', () => {
     if (item.length < 3) console.log('Customer name error! (word\'s length more than 3 symbol)');
   }
 })
-
+// checks for customer phone number field
+customerPhone.addEventListener('focus', () => {
+})
+customerPhone.addEventListener('blur', () => {
+  const phone: string = customerPhone.value;
+  const firstSymbol: string = phone[0];
+  if (firstSymbol != '+') {console.log('first symbol not "+');}
+  if ((phone.slice(1)).length < 9) {console.log('phone number less than 9 symbols');}
+  if (isNaN(Number(phone.slice(1)))) {console.log('phone number must contain only symbols')}
+})
