0fd2795c5ee9e89dc5f198cc7dbceecfb3b2ca21
chorny-znich <vargar.dt@gmail.com>
2023-01-06 03:03:41 +0300

    feat: add ProductDescriptionPage

--- online-store/src/app/index.ts
+++ online-store/src/app/index.ts
@@ -1,8 +1,8 @@
 import Page from '../common/page';
 import MainPage from '../pages/main-page';
-/*import CartPage from '../cart-page';
-import StatisticsPage from '../product-description-page';
-import Header from '../../core/components/header';*/
+//import CartPage from '../cart-page';
+import ProductDescriptionPage from '../pages/product-description-page';
+//import Header from '../../core/components/header';*/
 import ErrorPage from '../pages/404-page';
 
 export const enum PageIds {
@@ -27,9 +27,9 @@ class App {
       page = new MainPage(idPage);
     }/* else if (idPage === PageIds.CartPage) {
       page = new CartPage(idPage);
-    } else if (idPage === PageIds.ProductDescriptionPage) {
-      page = new StatisticsPage(idPage);
-    } */else {
+    }*/ else if (idPage === PageIds.ProductDescriptionPage) {
+      page = new ProductDescriptionPage(idPage);
+    } else {
       page = new ErrorPage(idPage);
     }
 
@@ -44,7 +44,7 @@ class App {
     } else if (idPage === PageIds.CartPage) {
       
     } else if (idPage === PageIds.ProductDescriptionPage) {
-      //App.showProductData();
+      App.showProductData();
     }
   }
 
@@ -55,7 +55,7 @@ class App {
     });
   }
 
-  /*private static showProductData() {
+  private static showProductData() {
     class Product {
       id: number;
       title: string;
@@ -189,7 +189,7 @@ class App {
       })
     }
   }
-*/
+
   constructor() {
   //  this.header = new Header('header', 'header-container');
   }
